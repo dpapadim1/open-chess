14c000b5ea2bfe69e508c21d2c8c4b771c5fed7b
dpapadim1 <dimitrispapadim@gmail.com>
2024-12-16 02:12:32 -0500

    deploy commit 15

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def newgame():
         return render_template("newgame.html")
 
 if __name__ == "__main__":
-    app.run(host='147.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
